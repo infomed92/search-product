9664e62d895e8a102b3dffc301f1551b4d2843e8
infomed92 <52166795+infomed92@users.noreply.github.com>
2020-11-24 19:11:10 +0100

    Delete allocation-template

--- shared/allocation-template.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IAllocationTemplate {
-    id?: number;
-    value?: any;
-    validityStart?: Date;
-    comment?: string;
-    allocationUnit?: string;
-    valueMinutes?: any;
-    valuehours?: any;
-}
