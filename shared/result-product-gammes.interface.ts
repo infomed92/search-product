54c9268f13798f285b92ac470c65943eee9d0386
infomed92 <52166795+infomed92@users.noreply.github.com>
2020-11-24 19:12:19 +0100

    Delete file

--- shared/result-product-gammes.interface.ts
+++
@@ -1,8 +0,0 @@
-import { IProduct } from 'app/pmt/shared/entity/search-product';
-import { IProductGammes } from './product-gammes.interface';
-
-export interface IResultProductGammes {
-    product?: IProduct;
-    n2Gammes?: IProductGammes[];
-    n1Gammes?: IProductGammes[];
-}
